6852308b351803b054af1e7480617f06aef8a06c
Adrien Thierry <adrien.thieryfr@gmail.com>
2016-12-23 17:30:20 +0100

    Updated

--- blog.conf.js
+++ blog.conf.js
@@ -173,6 +173,9 @@ module.exports =
       "custom_meta":
       [
         '<meta name="google-site-verification" content="q8fTBRiMegFYv-8Obtg8VFuO5IeR-a8oeGiw-AfMAis" />', // Google verification example, you can remove it
+        "<style>@media screen and (max-width: 1000px){.adsoptimal-slot{ margin:auto; width:300px!important; height:100px!important;}}</style>",
+        "<script type='text/javascript'>(function(w) {if(navigator.userAgent.match(/iPhone|iPod|iPad|Android/i)==null)return;var d=document,h=d.getElementsByTagName('head')[0],s=d.createElement('style'),j=d.createElement('script'),k=d.createElement('script');s.setAttribute('rel','mw-page-block');s.innerHTML='body > * {display:none !important}';j.setAttribute('src','//cdn.adsoptimal.com/advertisement/settings/54969.js');k.setAttribute('src','//cdn.adsoptimal.com/advertisement/dispatcher.js');j.onerror=k.onerror=function(){h.removeChild(s);h.removeChild(j);h.removeChild(k);};h.appendChild(s);h.appendChild(j);h.appendChild(k);})(window);</script>",
+        "<script type='text/javascript'>(function(w) {var d=document,h=d.getElementsByTagName('head')[0],j=d.createElement('script'),k=d.createElement('script');j.setAttribute('src','//cdn.adsoptimal.com/advertisement/settings/54969.js');k.setAttribute('src','//cdn.adsoptimal.com/advertisement/manual.js');h.appendChild(j); h.appendChild(k);})(window);</script>",
       ],
 
       "custom_script":
@@ -182,12 +185,12 @@ module.exports =
 
       "custom_left_column":
       [
-
+        '<div class="adsoptimal-slot" style="width: 160px; height: 600px;"></div>',
       ],
 
       "custom_footer_blog_top":
       [
-
+        '<div class="adsoptimal-slot" style="width: 728px; height: 90px;"></div>',
       ],
 
       "custom_footer_blog_bottom":
